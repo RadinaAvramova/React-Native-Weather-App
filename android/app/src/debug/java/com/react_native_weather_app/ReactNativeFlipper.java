12c8b8fb0fd6fa0c32546e507a106b2640fab542
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-07-06 20:17:57 +0300

    Update ReactNativeFlipper.java

--- android/app/src/debug/java/com/react_native_weather_app/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/react_native_weather_app/ReactNativeFlipper.java
@@ -1,9 +1,4 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
- * directory of this source tree.
- */
+
 package com.react_native_weather_app;
 
 import android.content.Context;
